ea191c0bbc7d91c370eea0b35e3fd147fa5fe8c7
magbanum <nighotshantanu@gmail.com>
2021-06-26 17:10:57 +0530

    Fixed

--- octoprofile/settings.py
+++ octoprofile/settings.py
@@ -78,12 +78,7 @@ WSGI_APPLICATION = 'octoprofile.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
