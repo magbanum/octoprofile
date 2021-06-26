003b41f84c1e8642ccbe7dbfe9e8d7088e743b4f
magbanum <nighotshantanu@gmail.com>
2021-06-26 17:24:11 +0530

    Fixed

--- octoprofile/settings.py
+++ octoprofile/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY =  'django-insecure-eqsm54$f&8$3=3l(=ehjg)17cj&qxnpt+&wm0gyj_4sryqz%6$'
+SECRET_KEY =  os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
