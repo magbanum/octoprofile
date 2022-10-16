9549766c04c935faf35709a1263822174ac25d46
Shantanu Nighot <shantanu.nighot@publicissapient.com>
2022-10-16 15:12:17 +0530

    allowed hosts changed

--- octoprofile/settings.py
+++ octoprofile/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['octoprofile.herokuapp.com']
+ALLOWED_HOSTS = ['octoprofile.up.railway.app']
 
 
 # Application definition
